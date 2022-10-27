3cb4e0316e115966c77ed669655e64013dc747e7
Katarzyna <katarzynahjdr@gmail.com>
2022-10-27 19:01:40 +0200

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gainsboro;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(467, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 108);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gainsboro;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(60, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 108);
+            this.label4.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Gainsboro;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(261, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 108);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Gainsboro;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(467, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 108);
+            this.label6.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Gainsboro;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(60, 485);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 108);
+            this.label7.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Gainsboro;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(261, 485);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 108);
+            this.label8.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Gainsboro;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(467, 485);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 108);
+            this.label9.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gainsboro;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(261, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 108);
+            this.label2.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 649);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 12;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gainsboro;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(60, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 108);
+            this.label1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(493, 9);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 14;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(276, 9);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 13);
+            this.label13.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(540, 641);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 671);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label10.Text = e.X + "; " + e.Y;
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string znak1 = "X";
+            string znak2 = "O";
+            if (e.Y < 200)
+            {
+                if (e.X < 200)
+                {
+                    if (label1.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole A1";
+                        label1.Text = znak1;
+                    }
+                }
+                else if (e.X > 400)
+                {
+                    if (label3.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole A3";
+                        label3.Text = znak1;
+                    }
+                }
+                else
+                {
+                    if (label2.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole A2";
+                        label2.Text = znak1;
+                    }
+                }
+            }
+            else if (e.Y > 400)
+            {
+                if (e.X < 200)
+                {
+                    if (label7.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole C1";
+                        label7.Text = znak1;
+                    }
+                }
+                else if (e.X > 400)
+                {
+                    if (label9.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole C3";
+                        label9.Text = znak1;
+                    }
+                }
+                else
+                {
+                    if (label8.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole C2";
+                        label8.Text = znak1;
+                    }
+                }
+            }
+            else
+            {
+                if (e.X < 200)
+                {
+                    if (label4.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole B1";
+                        label4.Text = znak1;
+                    }
+                }
+                else if (e.X > 400)
+                {
+                    if (label6.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole B3";
+                        label6.Text = znak1;
+                    }
+                }
+                else
+                {
+                    if (label5.Text != "") label11.Text = "Wybrane pole jest zajęte!";
+                    else
+                    {
+                        label11.Text = "Wybrano pole B2";
+                        label5.Text = znak1;
+                    }
+                }
+            }
+            if (((label1.Text == znak1) && (label2.Text == znak1) && (label3.Text == znak1)) || ((label7.Text == znak1) && (label8.Text == znak1) && (label9.Text == znak1)) || ((label1.Text == znak1) && (label4.Text == znak1) && (label7.Text == znak1)) || ((label3.Text == znak1) && (label6.Text == znak1) && (label9.Text == znak1)) || ((label1.Text == znak1) && (label5.Text == znak1) && (label9.Text == znak1)) || ((label3.Text == znak1) && (label5.Text == znak1) && (label7.Text == znak1)) || ((label2.Text == znak1) && (label5.Text == znak1) && (label8.Text == znak1)) || ((label4.Text == znak1) && (label5.Text == znak1) && (label6.Text == znak1)))
+            {
+                label13.Text = "GRACZ WYGRAŁ!";
+                pictureBox1.Enabled = false;
+            }
+            else
+            {
+                for (; ; )
+                {
+                    if ((label1.Text != "") && (label2.Text != "") && (label3.Text != "") && (label4.Text != "") && (label5.Text != "") && (label6.Text != "") && (label7.Text != "") && (label8.Text != "") && (label9.Text != ""))
+                    {
+                        break;
+                        pictureBox1.Enabled = false;
+                    }
+                    Random generator = new Random();
+                    int x = generator.Next(1, 10);
+                    switch (x)
+                    {
+                        case 1:
+                            if (label1.Text == "")
+                            {
+                                label1.Text = znak2;
+                                label12.Text = "Komputer wybrał pole A1";
+                                break;
+                            }
+                            else continue;
+                        case 2:
+                            if (label2.Text == "")
+                            {
+                                label2.Text = znak2;
+                                label12.Text = "Komputer wybrał pole A2";
+                                break;
+                            }
+                            else continue;
+                        case 3:
+                            if (label3.Text == "")
+                            {
+                                label3.Text = znak2;
+                                label12.Text = "Komputer wybrał pole A3";
+                                break;
+                            }
+                            else continue;
+                        case 4:
+                            if (label4.Text == "")
+                            {
+                                label4.Text = znak2;
+                                label12.Text = "Komputer wybrał pole B1";
+                                break;
+                            }
+                            else continue;
+                        case 5:
+                            if (label5.Text == "")
+                            {
+                                label5.Text = znak2;
+                                label12.Text = "Komputer wybrał pole B2";
+                                break;
+                            }
+                            else continue;
+                        case 6:
+                            if (label6.Text == "")
+                            {
+                                label6.Text = znak2;
+                                label12.Text = "Komputer wybrał pole B3";
+                                break;
+                            }
+                            else continue;
+                        case 7:
+                            if (label7.Text == "")
+                            {
+                                label7.Text = znak2;
+                                label12.Text = "Komputer wybrał pole C1";
+                                break;
+                            }
+                            else continue;
+                        case 8:
+                            if (label8.Text == "")
+                            {
+                                label8.Text = znak2;
+                                label12.Text = "Komputer wybrał pole C2";
+                                break;
+                            }
+                            else continue;
+                        case 9:
+                            if (label9.Text == "")
+                            {
+                                label9.Text = znak2;
+                                label12.Text = "Komputer wybrał pole C3";
+                                break;
+                            }
+                            else continue;
+                    }
+                    break;
+                }
+            }
+            if (((label1.Text == znak2) && (label2.Text == znak2) && (label3.Text == znak2)) || ((label7.Text == znak2) && (label8.Text == znak2) && (label9.Text == znak2)) || ((label1.Text == znak2) && (label4.Text == znak2) && (label7.Text == znak2)) || ((label3.Text == znak2) && (label6.Text == znak2) && (label9.Text == znak2)) || ((label1.Text == znak2) && (label5.Text == znak2) && (label9.Text == znak2)) || ((label3.Text == znak2) && (label5.Text == znak2) && (label7.Text == znak2)) || ((label2.Text == znak2) && (label5.Text == znak2) && (label8.Text == znak2)) || ((label3.Text == znak2) && (label4.Text == znak2) && (label5.Text == znak2)))
+            {
+                label13.Text = "KOMPUTER WYGRAŁ!";
+                pictureBox1.Enabled = false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Enabled = true;
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+            label10.Text = "";
+            label11.Text = "";
+            label12.Text = "";
+            label13.Text = "";
+        }
+    }
+}
